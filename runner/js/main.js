4d4a4c01891780b64e0d9864a455f6fdc145acd5
Brian Sunbury <bsunbury29@gmail.com>
2018-02-08 23:30:09 -0600

    mobile

--- runner/js/main.js
+++ runner/js/main.js
@@ -1,5 +1,5 @@
  //creates an instance of the game
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.WEBGL, '');
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '');
 
 game.state.add('Boot', StanRunner.Boot);
 game.state.add('Preloader', StanRunner.Preload);
